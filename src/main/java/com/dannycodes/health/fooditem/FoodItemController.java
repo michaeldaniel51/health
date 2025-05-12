c6eb4b3c2114b2890a4fd77f0eae8afbd415df5e
daniel michael <daniel.michael@bravewood.ng>
2025-05-12 16:12:45 +0100

    new updates to the food item like get item by id

--- src/main/java/com/dannycodes/health/fooditem/FoodItemController.java
+++ src/main/java/com/dannycodes/health/fooditem/FoodItemController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/food")
@@ -21,6 +22,11 @@ public class FoodItemController {
     @GetMapping
     public ResponseEntity<List<FoodItem>> getAllFood() {
         return ResponseEntity.ok(foodItemService.getAllFoodItems());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<FoodItem>> getFoodById(@PathVariable Long id) {
+        return ResponseEntity.ok(foodItemService.getFoodItemById(id));
     }
 
 }
